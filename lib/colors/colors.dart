6ce8be6f1e790ec811192411994d0e05a79957f3
larbiii14 <larbiii14@gmail.com>
2022-01-26 13:20:41 +0100

    Initial commit

---
+++ lib/colors/colors.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/widgets.dart';
+
+/// Defines the color palette for the puzzle UI.
+abstract class PuzzleColors {
+  /// Primary 1
+  static const Color primary1 = Color(0xFF041E3C);
+
+  /// Primary 2
+  static const Color primary2 = Color(0xFF042B59);
+
+  /// Primary 3
+  static const Color primary3 = Color(0xFF043875);
+
+  /// Primary 4
+  static const Color primary4 = Color(0xFF0553B1);
+
+  /// Primary 5  0468D7
+  static const Color primary5 = Color(0xFF7900FF);
+
+  /// Primary 6  027DFD
+  static const Color primary6 = Color(0xFF7900FF);
+
+  /// Primary 7  13B9FD
+  static const Color primary7 = Color(0xFFB172E3);
+
+  /// Primary 8 old color B8EAFE
+  static const Color primary8 = Color(0xFF90caf9);
+
+  /// Primary 9    old color F7F8FA
+  static const Color primary9 = Color(0xFF90caf9);
+
+  /// Primary 0   old color F7F8FA
+  static const Color primary0 = Color(0xFF90caf9);
+
+  /// Black
+  static const Color black = Color(0xFF000000);
+
+  /// Black 2 (opacity 20%)
+  static const Color black2 = Color(0x33000000);
+
+  /// Grey 1
+  static const Color grey1 = Color(0xFF4A4A4A);
+
+  /// Grey 2
+  static const Color grey2 = Color(0xFFA4A4A4);
+
+  /// Grey 3
+  static const Color grey3 = Color(0xFFF7F8FA);
+
+  /// Grey 4 #f7f8fa image color  F7F8FA
+  static const Color grey4 = Color(0xFF90caf9);
+
+  /// Grey 5  F7F8FA
+  static const Color grey5 = Color(0xFF90caf9);
+
+  /// White  F7F8FA
+  static const Color white = Color(0xFF90caf9);
+
+  /// White 2 (opacity 40%)
+  static const Color white2 = Color(0x66FFFFFF);
+
+  /// Green primary change to #541212
+  static const Color greenPrimary = Color(0xFF541212);
+
+  /// Green 90 change to #9D5353
+  static const Color green90 = Color(0xFF9D5353);
+
+  /// Green 50 change to #362222
+  static const Color green50 = Color(0xFF362222);
+
+  /// Blue primary change to #374046
+  static const Color bluePrimary = Color(0xFF374046);
+
+  /// Blue 90 change to #BDBDBD
+  static const Color blue90 = Color(0xFFBDBDBD);
+
+  /// Blue 50 change to #757575
+  static const Color blue50 = Color(0xFF757575);
+
+  /// Yellow primary change to #222831
+  static const Color yellowPrimary = Color(0xFF222831);
+
+  /// Yellow 90 change to #00ADB5
+  static const Color yellow90 = Color(0xFF186B98);
+
+  /// Yellow 50 change to #393E46
+  static const Color yellow50 = Color(0xFF393E46);
+}
